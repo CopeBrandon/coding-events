202fb572f830b9fa46e0fc62ea2ba847843afa25
Brandon Cope <hunterv96@gmail.com>
2022-05-19 15:21:02 -0500

    exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,27 +8,42 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+/**
+ * Created by Chris Bay
+ */
 @Controller
 @RequestMapping("events")
 public class EventController {
-
-    private static ArrayList<String> events = new ArrayList<>();
-
+    private static HashMap<String, String> events = new HashMap<>();
     @GetMapping
     public String displayAllEvents(Model model){
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         model.addAttribute("events", events);
         return "events/index";
     }
+//    private static List<String> events = new ArrayList<>();
+//    @GetMapping
+//    public String displayAllEvents(Model model) {
+//        model.addAttribute("title", "All Events");
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
 
-    @GetMapping("create")//lives at /events/create
-    public String renderCreateEventForm(){
+    @GetMapping("create")
+    public String displayCreateEventForm(Model model) {
+        model.addAttribute("title", "Create Event");
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String processCreateEventForm(@RequestParam String eventName) {
+        events.put(eventName, "test description");
         return "redirect:";
     }
+
 }
