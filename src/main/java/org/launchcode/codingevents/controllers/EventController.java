17ad4f68c33f1156ce97c2a70c28f0a300679f3a
Brandon Cope <hunterv96@gmail.com>
2022-05-19 14:31:00 -0500

    13.8

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,20 +3,20 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
+
+    private static ArrayList<String> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-        ArrayList<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strange Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -24,5 +24,11 @@ public class EventController {
     @GetMapping("create")//lives at /events/create
     public String renderCreateEventForm(){
         return "events/create";
+    }
+
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
     }
 }
